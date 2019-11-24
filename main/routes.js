ab6981009a0444ac618121e2223084093444023a
TamHOD <thomashodonnell@gmail.com>
2019-11-24 18:55:30 +0000

    add user profile routes

--- main/routes.js
+++ main/routes.js
@@ -83,4 +83,34 @@ router.delete("/api/delete/post", (req, res, next) => {
   });
 });
 
+router.post("/api/posts/userprofiletodb", (req, res, next) => {
+  const values = [
+    req.body.profile.nickname,
+    req.body.profile.email,
+    req.body.profile.email_verified
+  ];
+  pool.query(
+    `INSERT INTO users(username, email, email_verified, date_created)
+                VALUES($1, $2, $3, NOW())
+                ON CONFLICT DO NOTHING`,
+    values,
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
+});
+
+router.get("/api/get/userprofilefromdb", (req, res, next) => {
+  const email = req.query.email;
+  console.log(email);
+  pool.query(
+    `SELECT * FROM users
+                WHERE email=$1`,
+    [email],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
+});
+
 module.exports = router;
